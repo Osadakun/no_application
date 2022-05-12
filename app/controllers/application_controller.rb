39bec457b0803deb8d4bc86a1138c37a8b6b765b
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-05-13 00:37:51 +0900

    add file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+    # deviseの機能が使われる前に、configure_permitted_parametersを実行
+
+  protected
+  def configure_permitted_parameter
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email]) # sign_up時にユーザ名（+email）の操作を許可
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   resources :home
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-    # passwords: 'devise/passwords'
+  devise_for :users, :controllers => {
+    :confirmations => 'users/confirmations',
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
