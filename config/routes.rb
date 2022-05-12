cb57540624f8a5c4936c9fdbda76bc5f218d7585
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-05-12 23:49:58 +0900

    add file

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
-  }
+    passwords: 'devise/passwords'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
